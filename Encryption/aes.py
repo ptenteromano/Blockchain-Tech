9d4b88a1a00ab6c3d3ed306f285e3535e2d3a656
Philip Tenteromano <tenteromano.p@gmail.com>
2019-06-24 23:37:02 -0400

    README update, AES test

--- Encryption/aes.py
+++ Encryption/aes.py
@@ -1,22 +1,31 @@
 import subprocess
 
-inputFile = 'files/test.pdf'
-cipherFile = 'test.pdf.enc'
-plaintextPw = 'password'
+inputFile = "files/test.pdf"
+cipherFile = "test.pdf.enc"
+plaintextPw = "password"
 
 # Arguments List:
 # password, inputfile, output file
 
 try:
     # ENCRYPTING
-    subprocess.check_call(['./encrypt.sh', plaintextPw, inputFile, cipherFile])
+    subprocess.check_call(["./encrypt.sh", plaintextPw, inputFile, cipherFile])
+    with open(cipherFile, "r") as file:
+        data = file.read()  # .replace('\n', '')
 except:
     print("encyrpt")
 
+obj = {
+    'datastore': data
+}
+
+newCiphFile = 'new.pdf.enc'
+
+with open(newCiphFile, "w") as file:
+        file.write(data)
 try:
     # DECRYPTING
-    subprocess.check_call(['./decrypt.sh', plaintextPw, cipherFile, "output.pdf"])
+    subprocess.check_call(["./decrypt.sh", plaintextPw, newCiphFile, "output.pdf"])
 except:
-    print('decrypt')
+    print("decrypt")
 
-    
--- README.md
+++ README.md
@@ -9,5 +9,23 @@
 
 <strong>WIP</strong>
 
+### TODO:
+Nonce + TIMESTAMP
+- Make nonce 32bit intenger (~4 bill max)
+
+Finish Hybrid Encryption
+- We have the AES (symmetric) encrypts pdf with password
+- Need RSA public key to encrypt the password
+- Send: 
+    AES(document) + RSA_PUBLIC_Receiver(password) + RSA_PUBLIC_Sender(password) 
+    + HASH(originalPDF) + ?HASH(originalPW)?
+
+Get multiple servers up - connect them
+
+PERSIST the blockchain?
+- Possible database store?
+
+<hr>
+
 ### Goals:
 
