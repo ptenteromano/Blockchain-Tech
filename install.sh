c43b3bff891dae31136c5c9253267530f3dec236
Phil Tenteromano <philip.tenteromano@gmail.com>
2019-07-03 16:07:47 -0400

    virtual environment and commands to go with

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 # This file should do everything necessary to set up the app
 chmod u+x ./install.sh
 chmod u+x ./App/Encryption/*
+pip3 install -r ./requirements.txt || pip install -r ./requirements.txt
